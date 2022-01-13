b8edc125216cca76b1770761b3bf627334ede380
96Emil <91869024+96Emil@users.noreply.github.com>
2022-01-13 14:03:17 +0100

    Add files via upload

---
+++ array.js
@@ -0,0 +1,22 @@
+const products = [{
+    id: 1,
+    name: "",
+    price: 20,
+    image: "",
+    gender: "Man",
+    color: "White",
+    size: "10",
+    theme: "Summer"
+},
+  {
+    id: 2,
+    name: "",
+    price: 15,
+    image: "",
+    gender: "Woman",
+    color: "Blue",
+    size: "8",
+    theme: "Winter"
+}
+]
+
